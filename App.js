4af2d13f788097c58e4d76e9a2638854d7dd78bf
LINO-ANTONY <132889710+LINO-ANTONY@users.noreply.github.com>
2023-05-30 15:05:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import UserProfileDetails from './components/UserProfileDetails';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfileDetails/>
+      
+      
+    </div>
+  );
+};
+
+
+export default App;
---
+++ UserProfileDetails.jsx
@@ -0,0 +1,218 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Typography,
+  Avatar,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+} from '@mui/material';
+
+
+
+const UserProfileDetails = ({ userDetails, onUpdateDetails }) => {
+  const [name, setName] = useState(userDetails.name);
+  const [place, setPlace] = useState(userDetails.place);
+  const [age, setAge] = useState(userDetails.age);
+  const [email, setEmail] = useState(userDetails.email);
+  const [education, setEducation] = useState(userDetails.education);
+  const [phoneNumber, setPhoneNumber] = useState(userDetails.contactDetails.phoneNumber);
+  const [emailId, setEmailId] = useState(userDetails.contactDetails.emailId);
+  const [termsAndConditions, setTermsAndConditions] = useState(userDetails.termsAndConditions);
+
+  const handleUpdate = () => {
+    const updatedDetails = {
+      name,
+      place,
+      age,
+      email,
+      education,
+      contactDetails: {
+        phoneNumber,
+        emailId,
+      },
+      termsAndConditions,
+    };
+    onUpdateDetails(updatedDetails);
+  };
+
+  return (
+    <div>
+      <TextField
+        label="NAME"
+        fullWidth
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ marginBottom: '16px' }}
+      />
+      <TextField
+        label="PLACE"
+        fullWidth
+        value={place}
+        onChange={(e) => setPlace(e.target.value)}
+        style={{ marginBottom: '16px' }}
+      />
+      <TextField
+        label="AGE"
+        fullWidth
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+        style={{ marginBottom: '16px' }}
+      />
+      <TextField
+        label="EMAIL"
+        fullWidth
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{ marginBottom: '16px' }}
+      />
+      
+      <TextField
+        label="PHONE NUMBER"
+        fullWidth
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        style={{ marginBottom: '16px' }}
+      />
+      <TextField
+        label="CONTACT DETAILS"
+        fullWidth
+        value={emailId}
+        onChange={(e) => setEmailId(e.target.value)}
+        style={{ marginBottom: '16px' }}
+      />
+      <div style={{ marginBottom: '16px' }}>
+        <label htmlFor="termsAndConditions">
+          Terms and Conditions:
+          <input
+            id="termsAndConditions"
+            type="checkbox"
+            checked={termsAndConditions}
+            onChange={(e) => setTermsAndConditions(e.target.checked)}
+            style={{ marginLeft: '8px' }}
+          />
+        </label>
+      </div>
+      <Button variant="contained" color="primary" onClick={handleUpdate}>
+        Update
+      </Button>
+    </div>
+  );
+};
+
+const UserProfile = () => {
+  const [profilePic, setProfilePic] = useState('path_to_profile_image.jpg');
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+  const [loginDetails, setLoginDetails] = useState({
+    name: 'PAUL WALKER',
+    place: 'Sample Place',
+    age: 30,
+    email: 'john.doe@example.com',
+    education: 'Sample Education',
+    contactDetails: {
+      phoneNumber: '1234567890',
+      emailId: 'john.doe@example.com',
+    },
+    termsAndConditions: false,
+  });
+
+  const handleProfilePicChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      setProfilePic(e.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleOpenEditDialog = () => {
+    setOpenEditDialog(true);
+  };
+
+  const handleCloseEditDialog = () => {
+    setOpenEditDialog(false);
+  };
+
+  const handleUpdateProfileDetails = (updatedDetails) => {
+    setLoginDetails(updatedDetails);
+    handleCloseEditDialog();
+  };
+
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        backgroundColor: '#f5f5f5',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div style={{ marginBottom: '32px' }}>
+        <Avatar src={profilePic} alt="Profile Image" style={{ width: '140px', height: '140px', cursor: 'pointer' }} onClick={() => document.getElementById('profilePicInput').click()} />
+        <input type="file" id="profilePicInput" accept="image/*" style={{ display: 'none' }} onChange={handleProfilePicChange} />
+      </div>
+
+      <div
+        style={{
+          marginBottom: '32px',
+          backgroundColor: '#ffffff',
+          padding: '16px',
+          borderRadius: '8px',
+          boxShadow: '0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.06)',
+        }}
+      >
+        <Typography variant="h5" gutterBottom>
+          {loginDetails.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom>
+          {loginDetails.email}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Place: {loginDetails.place}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Age: {loginDetails.age}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Education: {loginDetails.education}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Phone Number: {loginDetails.contactDetails.phoneNumber}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Email ID: {loginDetails.contactDetails.emailId}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Terms and Conditions: {loginDetails.termsAndConditions ? 'Accepted' : 'Not Accepted'}
+        </Typography>
+        
+      </div>
+
+      <Button variant="outlined" color="primary" onClick={handleOpenEditDialog} style={{ marginTop: '16px' }}>
+          EDIT PROFILE
+        </Button>
+
+      <Button variant="contained" color="primary" style={{ marginTop: '32px' }}>
+        Logout
+      </Button>
+
+      <Dialog open={openEditDialog} onClose={handleCloseEditDialog}>
+        <DialogTitle>Edit Profile Details</DialogTitle>
+        <DialogContent>
+          <UserProfileDetails userDetails={loginDetails} onUpdateDetails={handleUpdateProfileDetails} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditDialog}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default UserProfile;
